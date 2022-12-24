6e34d1a8790c3760410ed0082ed96e76bf3466d1
Madhankumar08 <madhankumar2k@gmail.com>
2022-12-24 12:03:14 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css";
+import SideBar from "./components/Sidemenu";
+import TopBar from "./components/TopBar";
+import DashBoard from "./components/DashBoard";
+import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <SideBar />
+      <div id="content-wrapper" className="d-flex flex-column">
+        <TopBar />
+        <DashBoard />
+        <Footer />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
